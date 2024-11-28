2d7e2675aebfa66e8efde78b58ee56b2c0c14a9f
sapparapu <sushma.ora08@gmail.com>
2024-11-28 16:30:09 +0530

    worked on ec2_user data

--- userdata/main.tf
+++ userdata/main.tf
@@ -2,9 +2,8 @@ resource "aws_instance" "dev" {
     ami = "ami-0453ec754f44f9a4a"
     instance_type = "t2.micro"
     key_name = "new_key_us"
-  
-}
-resource "aws_s3_bucket" "dev" {
-    bucket = "vanbuc"
-  
+  user_data = file ("script.sh")
+  tags = {
+    name = "ec2_data"
+  }
 }
