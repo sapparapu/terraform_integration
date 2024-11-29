5d5798238d299f4c1e7a596dd8ef30545ba9eb76
sapparapu <sushma.ora08@gmail.com>
2024-11-29 17:33:51 +0530

    applied new changes

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -5,8 +5,8 @@ provider "aws" {
 
 
 resource "aws_key_pair" "example" {
-  key_name   = "new_key_us1"  # Replace with your desired key name
-  public_key = file("~/.ssh/id_rsa.pub") 
+  key_name   = "newkey_x"  # Replace with your desired key name
+  public_key = file("C:/Users/Kisho/.ssh/id_rsa.pub") 
 
 
 }
@@ -80,10 +80,11 @@ resource "aws_instance" "server" {
 
   connection {
     type        = "ssh"
-    user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
+    user        = "ec2-user"  # Replace with the appropriate username for your EC2 instance
     # private_key = file("C:/Users/veerababu/.ssh/id_rsa")
-    private_key = file("~/.ssh/id_rsa")  #private key path
-    host        = "self.public_ip"
+   private_key = file("C:/Users/Kisho/.ssh/id_rsa")
+   # private_key = file("~/.ssh/id_rsa")  #private key path
+    host        = self.public_ip
   }
   # local execution procee 
  provisioner "local-exec" {
@@ -93,7 +94,8 @@ resource "aws_instance" "server" {
   # File provisioner to copy a file from local to the remote EC2 instance
   provisioner "file" {
     source      = "file10"  # Replace with the path to your local file
-    destination = "/home/ubuntu/file10"  # Replace with the path on the remote instance
+    destination = "/home/ec2-user/file10"  # Replace with the path on the remote instance
+  #ssh -i "new_key_us1.pem" ec2-user@44.203.30.225
   }
   # remote execution process 
   provisioner "remote-exec" {
